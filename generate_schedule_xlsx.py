ac6f1f8d33d6b2b83c4253891820b7a4f5ceadef
a394szz <ngamell@mmm.com>
2021-09-03 13:46:01 -0500

    auto team name loading for schedules!

--- generate_schedule_xlsx.py
+++ generate_schedule_xlsx.py
@@ -3,12 +3,13 @@ import json
 import pandas as pd
 
 from libs.xlsx_utils import save_spreadsheet_to_file, cleanse_import_sheets
-from libs.league_functions import load_schedule
+from libs.league_functions import load_schedule, FFLeague
 
 LEAGUE_XLSX_PATH = os.path.join('content', 'Mixed-14 Fantasy Football League.xlsx')
 LEAGUE_OUTPUT_PATH = os.path.join('output', 'Mixed-14 Fantasy Football League.xlsx')
 SCHEDULE_PATH = os.path.join('output', 'schedule.json')
 
+LEAGUE = FFLeague()
 
 def generate_schedule_xlsx():
     if os.path.exists(LEAGUE_XLSX_PATH) and os.path.exists(SCHEDULE_PATH):
@@ -21,7 +22,7 @@ def generate_schedule_xlsx():
         with open(SCHEDULE_PATH, 'r') as sch_f:
             schedule_json = json.load(sch_f)
 
-        league_xlsx = load_schedule(league_xlsx, schedule_json)
+        league_xlsx = load_schedule(league_xlsx, schedule_json, LEAGUE)
 
         save_spreadsheet_to_file(league_xlsx, LEAGUE_OUTPUT_PATH)
 
--- libs/league_functions.py
+++ libs/league_functions.py
@@ -1,6 +1,18 @@
 import datetime
+import pprint
 
-def load_schedule(xlsx_dict: dict, schedule: dict) -> dict:
+import pandas as pd
+
+# Detailed API notes: https://github.com/cwendt94/espn-api/wiki/Football-Intro
+from espn_api.football import League
+
+from .config import CONFIG_SETTINGS
+
+def load_schedule(xlsx_dict: dict, schedule: dict, LEAGUE) -> dict:
+    LEAGUE.load_teams_from_espn(xlsx_dict['Teams'])
+    xlsx_dict['Teams'] = LEAGUE.update_teams_df(xlsx_dict['Teams'])
+    team_map = LEAGUE.get_teams()
+
     date_now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     for week in schedule["weeks"]:
         key = f"Week {week}"
@@ -13,12 +25,76 @@ def load_schedule(xlsx_dict: dict, schedule: dict) -> dict:
         xlsx_dict[key]["Score"].append("")
 
         for game in schedule["weeks"][week]:
-            xlsx_dict[key]["Team"].append(f"Team {game[0]}")
+            team1_key = f"Team {game[0]}"
+            team2_key = f"Team {game[1]}"
+            team1_name = team_map[team_map[team1_key]["map_id"]]["name"]
+            team2_name = team_map[team_map[team2_key]["map_id"]]["name"]
+            xlsx_dict[key]["Team"].append(team1_name)
             xlsx_dict[key]["Score"].append(0)
-            xlsx_dict[key]["Team"].append(f"Team {game[1]}")
+            xlsx_dict[key]["Team"].append(team2_name)
             xlsx_dict[key]["Score"].append(0)
             xlsx_dict[key]["Team"].extend(["", ""])
             xlsx_dict[key]["Score"].extend(["", ""])
 
 
     return xlsx_dict
+
+
+class FFLeague():
+
+    def __init__(self):
+        self.NE = League(
+            league_id=CONFIG_SETTINGS["league_id_ne"],
+            year=CONFIG_SETTINGS["year"],
+            espn_s2=CONFIG_SETTINGS["espn_s2"],
+            swid=CONFIG_SETTINGS["swid"]
+        )
+        self.SW = League(
+            league_id=CONFIG_SETTINGS["league_id_sw"],
+            year=CONFIG_SETTINGS["year"],
+            espn_s2=CONFIG_SETTINGS["espn_s2"],
+            swid=CONFIG_SETTINGS["swid"]
+        )
+        self.teams = dict()
+
+    def load_teams_from_espn(self, team_data=None):
+        if team_data is None:
+            return {}
+
+        # To map nicely with ESPN's order, we'll use the "MAP ID"
+        for i, _id in enumerate(team_data["MAP ID"]):
+            if _id != "":
+                self.teams[_id] = {
+                    "name": team_data["Team Name"][i],
+                    "owner": team_data["Owner"][i],
+                    "region": 'NE' if 'NE' in _id else 'SW',
+                    "team number": team_data["Team Number"][i]
+                }
+                region_id = int(_id.split('-')[1])
+                if self.teams[_id]["region"] == 'NE':
+                    if self.teams[_id]['name'] != self.NE.teams[region_id-1].team_name:
+                        self.teams[_id]['name'] = self.NE.teams[region_id-1].team_name
+                else:
+                    if self.teams[_id]['name'] != self.SW.teams[region_id-1].team_name:
+                        self.teams[_id]['name'] = self.SW.teams[region_id-1].team_name
+
+        # We need the reverse search to map "Team X" to the different divisions
+        temp_dict = dict()
+        for team_id in self.teams:
+            new_key = f"Team {self.teams[team_id]['team number']}"
+            temp_dict[new_key] = {
+                "map_id": team_id
+            }
+        self.teams.update(temp_dict)
+
+    def update_teams_df(self, team_data: pd.DataFrame) -> pd.DataFrame:
+        for i, team_id in enumerate(team_data["MAP ID"]):
+            if team_id == "":
+                continue
+            team_name = self.teams[team_id]["name"]
+            team_data["Team Name"][i] = team_name
+        return team_data
+
+    def get_teams(self):
+        return self.teams
+        
