ff61f5e4083e11e960ebd2bebf56e0b8ad2be397
a394szz <ngamell@mmm.com>
2021-09-08 00:00:25 -0500

    rosters loaded. First main release ready!

--- run_league_update.py
+++ run_league_update.py
@@ -2,9 +2,9 @@
 
 This will be the primary script run to update all scores, rosters, etc. for the "league".
 """
-from libs.xlsx_utils import LEAGUE_OUTPUT_PATH, load_league_spreadsheet, save_spreadsheet_to_file
+from libs.xlsx_utils import load_league_spreadsheet, save_spreadsheet_to_file
 from libs.league_functions import (
-    FFLeague, update_loaded_schedule, update_scores, update_standings
+    FFLeague, update_loaded_schedule, update_scores, update_standings, create_rosters
 )
 
 LEAGUE = FFLeague()
@@ -15,7 +15,7 @@ def run_league_update():
     league_xlsx = update_loaded_schedule(league_xlsx, LEAGUE)
     league_xlsx = update_scores(league_xlsx, LEAGUE)
     league_xlsx = update_standings(league_xlsx, LEAGUE)
-
+    league_xlsx = create_rosters(league_xlsx, LEAGUE)
 
     save_spreadsheet_to_file(league_xlsx)
 
