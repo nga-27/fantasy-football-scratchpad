600d10e44ac1a30a2e17106fa4df462d1ab2e58d
Matt <olber027@gmail.com>
2021-09-20 16:54:20 -0500

    changing argparse to actually use the proper name...damn copy-paste errors

--- generate_schedule_xlsx.py
+++ generate_schedule_xlsx.py
@@ -36,12 +36,12 @@ def generate_schedule_xlsx(schedule_path: Path, league_spreadsheet_path: Path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("update the league schedule")
     parser.add_argument("--schedule_path", "-s", required=False, default="output/schedule.json")
-    parser.add_argument("--output_path", "-l", required=False,
+    parser.add_argument("--league_spreadsheet_path", "-l", required=False,
                         default="output/Mixed-14 Fantasy Football League.xlsx")
     args = parser.parse_args()
 
     SCHEDULE_PATH = Path(args.schedule_path).resolve()
-    SPREADSHEET_PATH = Path(args.output_path).resolve()
+    SPREADSHEET_PATH = Path(args.league_spreadsheet_path).resolve()
 
     generate_schedule_xlsx(SCHEDULE_PATH, SPREADSHEET_PATH)
 
