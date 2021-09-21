3dcaa25e9d9106bf159e8b49e513e5ebb41e224f
a394szz <ngamell@mmm.com>
2021-09-20 19:57:07 -0500

    autoformat spreadsheet

--- libs/xlsx_utils.py
+++ libs/xlsx_utils.py
@@ -28,6 +28,19 @@ def save_spreadsheet_to_file(data: dict, output_file_path: Path):
                 data2.set_index(columns[0], inplace=True)
             data2.to_excel(writer, sheet_name=key)
 
+            # I am sick of formatting this constantly, so let's format this so column widths are 
+            # pretty and readable. Start with index (which is technically column 0, but it doesn't
+            # show up on "columns").
+            worksheet = writer.sheets[key]
+            max_len = find_max_column_width(data2.index)
+            worksheet.set_column(0, 0, max_len)
+            for idx, col in enumerate(data2):
+                max_len = find_max_column_width(data2[col])
+                # Add one since the index column is technically column 0 and is already done above.
+                worksheet.set_column(idx+1, idx+1, max_len)
+        
+        writer.save()
+
 
 def cleanse_import_sheets(data_sheet: pd.DataFrame) -> pd.DataFrame:
     """cleanse_import_sheets
@@ -62,3 +75,23 @@ def load_league_spreadsheet(spreadsheet_path: Path) -> dict:
         for sheet in xlsx.sheet_names:
             league_xlsx[sheet] = cleanse_import_sheets(xlsx.parse(sheet))
     return league_xlsx
+
+
+def find_max_column_width(column: list) -> int:
+    """find_max_column_width
+
+    Of all items in a given column list, find the "longest" item (when casted to a string) so we
+    can format the column widths appropriately.
+
+    Args:
+        column (list): column to evaluate each item for width by casting item to str and measuring        
+
+    Returns:
+        int: max length of column + 2
+    """
+    max_len = 0
+    for item in column:
+        if len(str(item)) > max_len:
+            max_len = len(str(item))
+
+    return max_len + 2
