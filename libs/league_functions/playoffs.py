44583f67af392c2d5fa952090aae765295bf371e
a394szz <ngamell@mmm.com>
2021-09-25 12:44:21 -0500

    fixed bracket object

--- libs/league_functions/playoffs.py
+++ libs/league_functions/playoffs.py
@@ -9,8 +9,9 @@ FORMAT = {
 }
 
 def load_playoffs(xlsx_dict: dict, playoff_data: dict, LEAGUE) -> dict:
-    print("playoffs")
     xlsx_dict["Playoffs-Wk1"] = copy.deepcopy(FORMAT)
+    xlsx_dict = load_round_one(xlsx_dict, playoff_data, LEAGUE)
+
     xlsx_dict["Playoffs-Wk2"] = copy.deepcopy(FORMAT)
     xlsx_dict["Playoffs-Wk3"] = copy.deepcopy(FORMAT)
     xlsx_dict["Playoffs-Wk4"] = copy.deepcopy(FORMAT)
@@ -20,4 +21,8 @@ def load_playoffs(xlsx_dict: dict, playoff_data: dict, LEAGUE) -> dict:
 
 def update_playoffs(xlsx_dict: dict, LEAGUE) -> dict:
     pprint.pprint(LEAGUE.get_rankings())
+    return xlsx_dict
+
+
+def load_round_one(xlsx_dict: dict, playoff_data: dict, LEAGUE) -> dict:
     return xlsx_dict
